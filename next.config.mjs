025904cd0090f26aa040613438e9907648207a7a
jerrimanishkumar <129955783+jerrimanishkumar@users.noreply.github.com>
2024-09-29 21:36:11 +0530

    cdsfdsffdsfds
    fdsf

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,6 @@ const nextConfig = {
     eslint: {
         ignoreDuringBuilds: true,
       },
-      reactStrictMode: true,
       typescript: {
         // !! WARN !!
         // Dangerously allow production builds to successfully complete even if
